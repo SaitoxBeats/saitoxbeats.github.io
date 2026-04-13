c565a3d953511aa55216651fb532ab533e84aeaf
Saitox <niko57gui@gmail.com>
2026-04-13 11:58:42 +0100

    ocmpres

--- wave.js
+++ wave.js
@@ -5,7 +5,7 @@ function animateWave() {
     t += 0.008;
     // Frequência baixa, mas amplitude de variação maior
     const freqX = 0.003 + Math.sin(t) * 0.0015;
-    const freqY = 0.004 + Math.cos(t * 0.7) * 0.002;
+    const freqY = 0.004 + Math.cos(t * 1.7) * 0.002;
     turb.setAttribute('baseFrequency', `${freqX} ${freqY}`);
     requestAnimationFrame(animateWave);
 }
